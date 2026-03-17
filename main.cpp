bc0670592449ad1969b4352fbd43747a3407a1f1
Sułkowski Jarosław <jaroslaw.sulkowski@pcosa.com.pl>
2026-03-17 09:52:43 +0100

    YUY2 works with 1280x720 at 52 FPS

--- main.cpp
+++ main.cpp
@@ -6,12 +6,15 @@
 #include <opencv2/video/tracking.hpp>
 #include <iostream>
 
+// #define FACE_DETECT
+
 int main()
 {
 	cv::VideoCapture cap(
 			"v4l2src device=/dev/video2 ! "
-			"video/x-raw,width=1280,height=720,framerate=30/1 ! "
-			"videoconvert ! video/x-raw,format=YUY2 ! appsink",  // imxvideoconvert_g2d
+			"video/x-raw,format=YUY2,width=1280,height=720,framerate=50/1 ! "
+			"appsink",
+			// "videoconvert ! video/x-raw,format=YUY2 ! appsink",  // imxvideoconvert_g2d
 			cv::CAP_GSTREAMER);
 	if (!cap.isOpened())
 	{
@@ -33,18 +36,18 @@ int main()
 	cv::Mat frame, gray, edges;
 	cap >> frame;
 
-	std::cout << frame.cols << "x" << frame.rows << " " << 
-			frame.type() << " " << frame.channels() << "\n";
+	printf("%dx%d, type=%d, channels=%d\n", frame.cols, frame.rows,
+			frame.type(), frame.channels());
 
 	std::string pipeline =
 			"appsrc is-live=true do-timestamp=true format=time ! "
-			"video/x-raw,width=" + std::to_string(frame.cols) + // format=BGR
-					",height=" + std::to_string(frame.rows) + ",framerate=30/1 ! " +
+			"video/x-raw,format=BGR,width=" + std::to_string(frame.cols) +
+					",height=" + std::to_string(frame.rows) + " ! " +
 			"videoconvert ! " +
-			"waylandsink sync=false window-width=" +
-					std::to_string(frame.cols) + " window-height=" + std::to_string(frame.rows);
-			// "fpsdisplaysink sync=false video-sink=\"waylandsink sync=false window-width=" +
-			// 		std::to_string(frame.cols) + " window-height=" + std::to_string(frame.rows) + "\"";
+			// "waylandsink sync=false window-width=" +
+			// 		std::to_string(frame.cols) + " window-height=" + std::to_string(frame.rows);
+			"fpsdisplaysink sync=false video-sink=\"waylandsink sync=false window-width=" +
+					std::to_string(frame.cols) + " window-height=" + std::to_string(frame.rows) + "\"";
 
 	cv::VideoWriter out(pipeline, cv::CAP_GSTREAMER, 0, 30, frame.size(), true);
 
@@ -54,40 +57,43 @@ int main()
 		return -1;
 	}
 
+	struct timespec last_ts;
 	while (true)
 	{
 		struct timespec ts;
 		clock_gettime(CLOCK_REALTIME, &ts);
 		struct tm *tm_info = localtime(&ts.tv_sec);
 		int us = ts.tv_nsec / 1000;
+		int diff_us = (ts.tv_nsec - last_ts.tv_nsec) / 1000;
+		if (diff_us < 0)
+			diff_us += 1000000;
+		memcpy(&last_ts, &ts, sizeof(ts));
 
-		std::cout << "frame " << us / 1000 << " " << frame.type() << " " << frame.channels() << "\n";
+		printf("%2ld.%03d ms (+%2d ms)\n", ts.tv_sec % 60, us / 1000, diff_us / 1000);
 
 		cap >> frame;
 		if (frame.empty())
 			break;
 
+#ifdef FACE_DETECT
 		// konwersja do grayscale
 		// cv::cvtColor(frame, gray, cv::COLOR_BGR2GRAY);
+		cv::cvtColor(frame, gray, cv::COLOR_YUV2GRAY_YUY2);
 
-		// // wykrywanie krawędzi
-		// cv::Canny(gray, edges, 80, 150);
+		// wykrywanie krawędzi
+		cv::Canny(gray, edges, 80, 150);
 
-		// // wykrywanie twarzy
-		// std::vector<cv::Rect> faces;
-		// face_cascade.detectMultiScale(gray, faces);
+		// wykrywanie twarzy
+		std::vector<cv::Rect> faces;
+		face_cascade.detectMultiScale(gray, faces);
 
-		// for (const auto &f : faces)
-		// 	cv::rectangle(frame, f, cv::Scalar(0,255,0), 2);
+		for (const auto &f : faces)
+			cv::rectangle(frame, f, cv::Scalar(0,255,0), 2);
+#endif
 
-		out.write(frame);
-
-// cv::Mat bgr;
-// if(frame.channels() == 1)
-//     cv::cvtColor(frame, bgr, cv::COLOR_YUV2BGR_YUY2);
-// else
-//     bgr = frame;
-// out.write(bgr);
+		cv::Mat bgr;
+		cv::cvtColor(frame, bgr, cv::COLOR_YUV2BGR_YUY2);
+		out.write(bgr);
 
 		if (cv::waitKey(1) == 27) // ESC
 			break;
